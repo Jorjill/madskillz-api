ad6d1db7ab0901ff36adac45a3c6fb03fc6678bc
Jorjill <orgil9506@gmail.com>
2024-05-27 23:26:27 -0500

    prepare for heroku

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,6 +1,5 @@
 // firebaseAdmin.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./madskillz-a6ba6-firebase-adminsdk-9f6ai-4d04fe69d9.json");
 
 admin.initializeApp({
   credential: admin.credential.cert({
